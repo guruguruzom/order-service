9f82b9f087632321c2d5275467754a0c8385e208
guruguruzom <guruguruzom@gmail.com>
2023-07-23 18:11:50 +0900

    kafka - order db 동기화

--- src/main/java/com/guruguruzom/orderservice/controller/OrderController.java
+++ src/main/java/com/guruguruzom/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.guruguruzom.orderservice.controller;
 import com.guruguruzom.orderservice.dto.OrderDto;
 import com.guruguruzom.orderservice.entity.OrderEntity;
 import com.guruguruzom.orderservice.messagequeue.KafkaProducer;
+import com.guruguruzom.orderservice.messagequeue.OrderProducer;
 import com.guruguruzom.orderservice.service.OrderService;
 import com.guruguruzom.orderservice.vo.RequestOrder;
 import com.guruguruzom.orderservice.vo.ResponseOrder;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +27,7 @@ public class OrderController {
     private final Environment env;
     private final OrderService orderService;
     private final KafkaProducer kafkaProducer;
+    private final OrderProducer orderProducer;
 
     @GetMapping("/health-check")
     public String status(){
@@ -36,16 +39,23 @@ public class OrderController {
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId, @RequestBody RequestOrder orderDetails){
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-        /* jpa */
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
-        OrderDto createOrder = orderService.createOrder(orderDto);
+        /* jpa */
+        //OrderDto createOrder = orderService.createOrder(orderDto);
+        //ResponseOrder responseOrder = mapper.map(createOrder, ResponseOrder.class);
 
-        ResponseOrder responseOrder = mapper.map(createOrder, ResponseOrder.class);
+        /* kafka */
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+
 
         /* send this order to the kafka */
         kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
+
+        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
 
     }
